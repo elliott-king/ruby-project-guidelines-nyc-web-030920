8fe599cc0570b836e7e920a178d3886fa08f2376
Yueying <wangyy395@gmail.com>
2020-03-26 14:38:15 -0400

    disable rails logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'date'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-ActiveRecord::Base.logger.level = 1
+
 # require_all 'lib'
 require_all 'app/model'
 ActiveRecord::Base.logger = nil
